d36497d281dca636d003e96924d8f1029f4766f3
Rino <rinooktavianridwan@gmail.com>
2024-02-19 06:45:46 +0700

    responsive v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { Routes, Route } from 'react-router-dom'
-
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
-import Home from './pages/home/Home'
-import MyPokemon from './pages/mypokemon/MyPokemon'
-import PokemonList from './pages/pokemonlist/PokemonList'
-import Preloader from './components/preloader/Preloader'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import MyPokemon from "./pages/mypokemon/MyPokemon";
+import PokemonList from "./pages/pokemonlist/PokemonList";
+import Preloader from "./components/preloader/Preloader";
 
 function App() {
+  const isWideScreen = window.innerWidth >= 1024;
 
   return (
     <div>
-      <Preloader />
       <Navbar />
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/PokemonList" Component={PokemonList} />
         <Route path="/MyPokemon" Component={MyPokemon} />
       </Routes>
+      {isWideScreen && <Preloader />}
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
